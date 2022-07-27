31558dabc4e14acfc82d0b8ad55d3d49f0a4862f
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-27 02:43:09 -0400

    add more code not done

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,4 +1,5 @@
 import random
+form collections import Counter
 
 
 
@@ -6,8 +7,33 @@ class GameLogic:
     def __init__(self):
         pass
 
-@staticmethod
-def roll_dice(num):
-    rolls = []
-    while len(rolls): < num:
-        rolls.append(random.randint)
+    @staticmethod
+    def roll_dice(num):
+        rolls = []
+        while len(rolls) < num:
+            rolls.append(random.randint(1, 6))
+
+        return tuple(rolls)
+
+    @staticmethod
+    def calculate_score(tup):
+        total = 0
+        roll = Counter(tup).most_common()
+
+        if len(tup) == 0:
+            return total
+
+
+        print(f"print roll: {roll}")
+        
+        if len(roll) == 6:
+            total += 1500
+
+        if roll [0][1] == 6:
+            if roll[0][0] == 1:
+                total += 4000
+                return total
+            total +- int
+
+
+
