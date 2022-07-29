bfbbf574ae72b15b3fdc6147b30e80535553b883
RIKI PLAZA <rikiplaza1@gmail.com>
2022-07-29 02:50:25 -0400

    not done, added more code

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,11 +1,11 @@
 import random
-form collections import Counter
-
+from collections import Counter
 
 
 class GameLogic:
     def __init__(self):
         pass
+
 
     @staticmethod
     def roll_dice(num):
@@ -25,7 +25,7 @@ class GameLogic:
 
 
         print(f"print roll: {roll}")
-        
+
         if len(roll) == 6:
             total += 1500
 
@@ -33,7 +33,26 @@ class GameLogic:
             if roll[0][0] == 1:
                 total += 4000
                 return total
-            total +- int
+            total += int(roll[0][0]) * 400
+            return total
+
+        elif roll[0][1] == 5:
+            if roll[1][0] == 1:
+                total += 3000
+
+
+            elif len (roll) > 1:
+                if roll[1][0] == 5:
+                    total += 50
+
+                elif roll[1][0] == 1:
+                    total += 100
+
+
+            else:
+                total += int(roll[0][0]) * 300
+                return total
+
 
 
 
